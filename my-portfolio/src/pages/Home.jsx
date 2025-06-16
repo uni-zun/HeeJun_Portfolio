4cebe5050e507b834b34cc822021acbe2f15ebd2
Yoon Hee Jun <yhj0566@gmail.com>
2025-06-16 23:16:49 +0900

    기본세팅

---
+++ my-portfolio/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-purple-600 to-pink-500">
+      <h1 className="text-4xl font-bold text-white">Tailwind ✔️</h1>
+    </div>
+  )
+}
+
+export default Home
