84bcedc3f7610827ba89f6d469d60a31ba2e7a03
Yoon Hee Jun <yhj0566@gmail.com>
2025-06-18 01:29:22 +0900

    devleop

--- my-portfolio/src/pages/Home.jsx
+++ my-portfolio/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div className="h-screen flex items-center justify-center bg-gradient-to-r from-purple-600 to-pink-500">
-      <h1 className="text-4xl font-bold text-white">Tailwind ✔️</h1>
+      <h1 className="text-4xl font-bold text-white">Tailwind1 ✔️</h1>
     </div>
   )
 }
